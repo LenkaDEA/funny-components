517d0a0ea21959e33ea3fa8cbe75b6c67fa7e2df
Elena Diatlenko <lenadea199900@gmail.com>
2025-09-01 19:42:54 +0300

    feat: add clicks

--- src/pages/GameXO/GameXO.tsx
+++ src/pages/GameXO/GameXO.tsx
@@ -22,16 +22,28 @@ const fildItems: fildItemType[] = [
 
 const GameXO: React.FC = () => {
     const [fild, setFild] = useState(fildItems);
+    const [player, setPlayer] = useState(false); // false - X; true - 0
 
     const handleClick = (id: number) => {
         console.log('click', id);
 
+        setPlayer(!player);
+        console.log('Походил игрок:', player)
+
         const copyFild = fild.map(item => item);
         copyFild.map((item) => {
-            if (item.id === id) item.state = "Q";
+            if (item.id === id) { player ? item.state = "O" : item.state = "X" };
         });
-
         setFild(copyFild);
+    }
+
+    const cleanFild = () => {
+        const copyFild = fild.map(i => i);
+        copyFild.map((item) => {
+            item.state = null;
+        })
+        setFild(copyFild);
+        setPlayer(false);
     }
 
     return (<div className={styles.game}>
@@ -39,8 +51,8 @@ const GameXO: React.FC = () => {
 
         <p className={styles.game__turn}>Ход игрока</p>
         <div className={styles.game__players}>
-            <p className={styles.game__players_name}>Игрок X</p>
-            <p className={styles.game__players_name}>Игрок O</p>
+            <p className={player ? styles.game__players_name : styles.game__players_name_lead}>Игрок X</p>
+            <p className={player ? styles.game__players_name_lead : styles.game__players_name}>Игрок O</p>
         </div>
 
         <div className={styles.game__fild}>
@@ -48,11 +60,19 @@ const GameXO: React.FC = () => {
                 <button
                     key={item.id}
                     className={styles.game__fild_button}
-                    onClick={() => { handleClick(item.id) }}
+                    onClick={() => {
+                        if (!item.state) { handleClick(item.id) }
+                        else { console.log('Поле занято!') }
+                    }}
                 >
                     {item.state}
                 </button>)}
         </div>
+
+        <button
+            className={styles.game__clean}
+            onClick={() => { cleanFild(); }}
+        >Начать заново</button>
     </div>);
 };
 
